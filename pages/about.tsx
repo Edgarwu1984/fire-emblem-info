c9ed5d1ee28930059c6133fa4b8946e81324f532
Edgarwu1984 <wuzhengjie@gmail.com>
2022-05-08 21:45:07 +1000

    Change file name

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from 'next';
 import React from 'react';
-import Layout from '../components/Layout/Layout';
+import Layout from '../components/Layout';
 
 const AboutPage: NextPage = () => {
   return (
